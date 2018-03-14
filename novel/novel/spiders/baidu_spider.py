ea96abb1212dcbd211ad9a8e41b10dd4d3cc7a20
jime <282119459@qq.com>
2018-03-14 09:27:36 +0800

    update

--- novel/novel/spiders/baidu_spider.py
+++ novel/novel/spiders/baidu_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 import re
+from urllib.parse import urlparse
 from scrapy import Request
 class BaiduSpider(scrapy.Spider):
     name = 'baidu'
@@ -8,9 +9,11 @@ class BaiduSpider(scrapy.Spider):
     def parse(self,response):
         for sel in response.xpath('//div[contains(@class,"results")]/div[contains(@id,"sogou_vr_")]') :
             #去除搜狗正版小说结果，要钱的不要
-            if len(sel.xpath('.//img[contains(@class,"vr_authico")]').extract()) == 0  :
+            #if len(sel.xpath('.//img[contains(@class,"vr_authico")]').extract()) == 0  :
+            if ''.join(sel.xpath('.//a[contains(@id,"title")]/@href').extract()).find('sogou') < 0 :
+                novelInfo = re.findall("作者：(.*)类型：(.*)状态：(.*)简介：(.*)新：",''.join(sel.xpath('.//text()').extract()).replace('\n','').replace(' ',''))
+                print(novelInfo)
                 print(sel.xpath('.//a[contains(@id,"title")]/@href').extract())
-                #print(sel.xpath('.//div[contains(@class,"str-box")]'))
-                #print(''.join(sel.xpath('.//text()').extract()))
-                print(re.findall("作者：(.*)类型：(.*)状态：(.*)简介：(.*)新：",''.join(sel.xpath('.//text()').extract()).replace('\n','').replace(' ','')))
-
+                for s in sel.xpath('.//a[contains(@id,"title")]/@href').extract() :
+                    res = urlparse(s).netloc
+                    print(res)
